48c98f0b03ee98018ebf60f9c4b865aa947e83d8
queensaikia <queensaikia98@gmail.com>
2019-07-15 10:44:07 -0700

    commit

---
+++ src/containers/SingleSeries/index.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Loader from '../../components/Loader';
+
+class SingleSeries extends Component {
+  state = {
+    show:!null
+  }
+
+componentDidMount() {
+  const { id } = this.props.match.params;
+
+fetch(`http://api.tvmaze.com/shows/${id}?embed=episodes`)
+.then(response => response.json())
+.then(json => this.setState({ shows: json }));
+}
+
+  render() {
+    const { show } = this.state;
+console.log(show);
+
+    return (
+      <div>
+        { show === null && <Loader />}
+        {
+
+           show !== null
+            &&
+          <p>Show has been loaded</p>
+
+        }
+</div>
+    )
+
+      }
+
+    }
+
+
+export default SingleSeries;
